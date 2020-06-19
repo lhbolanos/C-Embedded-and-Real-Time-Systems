dd1e62af6550d911e795538f04af96ad3e096bd8
Lucas Bolanos <67168276+lhbolanos@users.noreply.github.com>
2020-06-19 14:19:22 -0700

    Update BloomFilter.h

--- Bloom Filter and Hash Functions/BloomFilter.h
+++ Bloom Filter and Hash Functions/BloomFilter.h
@@ -1,15 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/* 
- * File:   BloomFilter.h
- * Author: lhbol
- *
- * Created on October 28, 2019, 7:42 PM
- */
 
 #ifndef BLOOMFILTER_H
 #define BLOOMFILTER_H
